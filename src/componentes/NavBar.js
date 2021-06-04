e4a4abdc87c1e75928f9365fbbea8d5474faed5e
jorgeduran1996 <jorgeduran1596@gmail.com>
2021-06-03 23:53:22 -0300

    NavBar

---
+++ src/componentes/NavBar.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+
+
+
+export default function NavBar()
+{return(
+    <NavBar bg="light">
+            <NavBar.Brand href="#home">NipFly</NavBar.Brand>
+            <NavBar.Brand href="#about">About</NavBar.Brand>
+            <NavBar.Brand href="#contactos">Contactoas</NavBar.Brand>
+
+
+    </NavBar>
+)
+
+
+}
